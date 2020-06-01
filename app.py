55017db3bd9aeff33800960712ce291f436f3305
Steven <stransteven@outlook.com>
2020-06-01 16:50:09 +1000

    Added redis keystore

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+import os
+import redis
+from flask import Flask, request
 
 app = Flask(__name__)
+r = redis.Redis(host=os.environ.get('REDIS_URL', 'http://localhost:6379'))
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+@app.route('/', methods=['GET'])
+def get_value():
+    key = request.args.get('key')
+    return r.get(key) or 'No value found'
+
+
+@app.route('/', methods=['POST'])
+def set_value():
+    key = request.args.get('key')
+    value = request.args.get('value')
+    if key and value:
+        r.set(key, value)
+    return 'Saved'
