e421cc967b98d3083613da7b5dd7059b4c99fbf8
Steven <stransteven@outlook.com>
2020-06-02 15:27:12 +1000

    Made app more friendly for Kubernetes deploy

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
 import os
-import redis
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
-r = redis.Redis(host=os.environ.get('REDIS_URL', 'http://localhost:6379'))
 
-@app.route('/', methods=['GET'])
-def get_value():
-    key = request.args.get('key')
-    return r.get(key) or 'No value found'
-
-
-@app.route('/', methods=['POST'])
-def set_value():
-    key = request.args.get('key')
-    value = request.args.get('value')
-    if key and value:
-        r.set(key, value)
-    return 'Saved'
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
